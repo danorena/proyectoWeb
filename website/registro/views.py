6d84e65131d3c9d36c1c44cae9c27ad0fea931a6
David Noreña <danorena755@misena.edu.co>
2022-06-11 12:45:20 -0500

    Vista login y registro, sin db

---
+++ website/registro/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+import mysql.connector as sql
+
+user = ''
+email = ''
+password = ''
+
+ 
+# Create your views here.
+def callRegister(request):
+    pass
+    global user,email,s,em,pwd 
+    if request.method == 'POST':
+        m = sql.connector(host='localhost',user='root',passwd='',database='usuarios')
+        cursor = m.cursor()
+        d= request.POST
+        for key,value in d.items():
+            if key=='user':
+                user = value
+            if key== 'email':
+                email = value
+            if key == 'password':
+                password = value
+                
+        c = f"CALL `spInsertUser`('{user}', '{email}', '{password}');"
+        cursor.execute(c)
+        m.commit()
+        
+    return render(request,'register.html')
+            
