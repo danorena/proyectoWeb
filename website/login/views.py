cf6f84d00e556f524030626727e41ca22cd91dd5
David Noreña <danorena755@misena.edu.co>
2022-06-14 21:31:02 -0500

    login incompleto, casi listo

--- website/login/views.py
+++ website/login/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
+import mysql.connector as sql
+
+user = ''
+password = ''
 
 # Create your views here.
 def callLogin(request):
-    return render(request,'login.html')
-    # return None
+    global user,password 
+    if request.method == 'POST':
+        m = sql.connect(host='localhost',user='root',passwd='',database='usuarios')
+        cursor = m.cursor()
+        d= request.POST
+        for key,value in d.items():
+            # Valida usuario
+            if key=='user':
+                user = value
+            # Valida pw
+            if key == 'password':
+                password = value
+                
+        c = f"CALL `spLogin`('{user}', '{password}');"
+        cursor.execute(c)
+        m.commit()
+        
+        valid = tuple(cursor.fetchall())
+        if t == ():
+            return (request,'login.html')
+        else:
+            return render(request,'index.html')
+            
+        
+    
+ 
+            
