6a1f4c48de6e56907bddbbc695290ae48e0be2a3
David <Danorena755@misena.edu.co>
2022-06-29 11:20:40 -0500

    Lectura de fichas completa

--- website/asistencia/views.py
+++ website/asistencia/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+
 
 # Create your views here.
 def asistenciaCall(request):
-    return render(request,'asistencia.html')
+    fichas = dir()
+    return render(request,'asistencia.html',{'fichas' : fichas})
 
 # Hacemos una funcion para leer las fichas
 def dir():
@@ -11,18 +12,7 @@ def dir():
     # buscamos la ruta que contiene todas la fichas
     path  = 'C:\\Users\David\Desktop\proyectoWeb\\fichas'
     # guardamos todas las fichas en una lista iterable
-    dir_list = os.listdir(path)
+    grupo = os.listdir(path)
     # Devolvemos una lista para iterar sobre esta 
-    return dir_list
+    return grupo
 
-def python_funct(request):
-      #do something with the data passed
-
-      fname=request.GET.get('fname',None)
-      lname=request.GET.get('lname',None)
-      fullname= fname+' '+lname
-      #fullname= 33
-      response = {
-        'fullname': fullname
-         }
-      return JsonResponse(response)
