35f99b82aee0c7d587e3adfd4723cecea32634b7
David Noreña <danorena755@misena.edu.co>
2022-06-29 19:38:01 -0500

    Ruta dinamica lista

--- website/asistencia/views.py
+++ website/asistencia/views.py
@@ -6,13 +6,36 @@ def asistenciaCall(request):
     fichas = dir()
     return render(request,'asistencia.html',{'fichas' : fichas})
 
+# Funcion para obtener la ruta
+def ruta():
+    import os
+    # Obtenemos la ruta actual
+    path = os.getcwd()
+    # Partimos todo lo que tenga el backslash \\ y nos retorna una lista
+    path = path.split('\\')
+    # Creamos una lista
+    entirePath = []
+    # recorremos la lista de la ruta.
+    for p in path:
+        if (p == 'web'):
+            break
+        else:
+            p += '/'
+            entirePath.append(p)
+    entirePath.append('attendance/')
+    letter = ''    
+    path = letter.join(entirePath)
+    path += 'model/datasets/attendance_system_dataset'
+    return path
+
 # Hacemos una funcion para leer las fichas
 def dir():
     import os
     # buscamos la ruta que contiene todas la fichas
-    path  = 'C:\\Users\David\Desktop\proyectoWeb\\fichas'
+    path  = ruta() 
     # guardamos todas las fichas en una lista iterable
     grupo = os.listdir(path)
     # Devolvemos una lista para iterar sobre esta 
     return grupo
+
 
