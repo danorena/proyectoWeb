d91df0e430f234974aef1eb889da86bfdb2ed9fa
Sebastian Salas <sebsalas@misena.edu.co>
2022-07-03 15:13:56 -0500

    configuracion de la cuenta view and path call

---
+++ website/static/configuracion/js/main.js
@@ -0,0 +1,6 @@
+function redireccion(){
+    formulario  = document.getElementById('miBuscador');
+
+    if(formulario==1)
+        location.href = "../../../../../../proyecto-sena/attendance/model/datasets/attendance_system_dataset/01/database"
+}
